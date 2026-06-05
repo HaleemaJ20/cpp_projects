e8525ffde20c0cf846b652a68de59c7322a5741e
HaleemaJ20 <haleemajaseem93@gmail.com>
2026-06-06 01:18:39 +0500

    Defines Student class structure and attributes

--- Student.h
+++ Student.h
@@ -3,7 +3,7 @@
 #include <iomanip>
 using namespace std;
 
-class Student{
+class Student {
 private:
     int id;
     string name;
@@ -11,15 +11,19 @@ private:
     string department;
 
 public:
+//Default Constructor
     Student();
 
+//Parameterized Constructor
     Student(int i, string n, float m, string d);
 
+//getters
     int getId();
     string getName();
     float getMarks();
     string getDepartment();
 
+//setters
     void setName(string n);
     void setMarks(float m);
     void setDepartment(string d);
